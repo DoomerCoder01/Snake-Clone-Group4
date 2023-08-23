324eb4e573c1180b9f88067c1ae4ac5b0d1043e3
CyberWolf4895 <140253190+CyberWolf4895@users.noreply.github.com>
2023-08-23 16:03:57 +0200

    Various Scenes + UI changes

---
+++ Snake Clone/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Gives players access to a pause menu <Reference: Brackeys(2017).PAUSE MENU in Unity.Available at: https://www.youtube.com/watch?time_continue=696&v=JivuXdrIHK0&feature=emb_logo (Date acceseed: 05 June 2020).>
+    /// </summary>
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
